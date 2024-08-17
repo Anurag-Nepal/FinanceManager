a6d69a42281604ec9a3869844d1e786da0de64c5
Anurag-Nepal <anuragnepal99@gmail.com>
2024-08-17 11:28:59 +0545

    again

--- src/main/java/com/pennywisenepal/financetracker/Config/WebConfig.java
+++ src/main/java/com/pennywisenepal/financetracker/Config/WebConfig.java
@@ -8,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Order(1)
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("Content-Type", "Accept", "Accept-Language", "Accept-Encoding")
+                .exposedHeaders("Location")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
