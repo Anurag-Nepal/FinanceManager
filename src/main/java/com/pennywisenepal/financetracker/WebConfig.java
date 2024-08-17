3cc3c1e4da51eca830ef9bb2df3387f0c2104bf6
Anurag-Nepal <anuragnepal99@gmail.com>
2024-08-17 12:07:31 +0545

    2223123123123123123

--- src/main/java/com/pennywisenepal/financetracker/WebConfig.java
+++ src/main/java/com/pennywisenepal/financetracker/WebConfig.java
@@ -1,7 +1,5 @@
-package com.pennywisenepal.financetracker.Config;
-import org.springframework.context.annotation.Bean;
+package com.pennywisenepal.financetracker;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
