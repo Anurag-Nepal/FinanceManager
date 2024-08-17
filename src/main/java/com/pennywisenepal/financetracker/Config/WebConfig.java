938a4313784d27ea66db242403bc37fdcaa109ce
Anurag-Nepal <anuragnepal99@gmail.com>
2024-08-17 10:03:50 +0545

    Webconfig class comiited

--- src/main/java/com/pennywisenepal/financetracker/Config/WebConfig.java
+++ src/main/java/com/pennywisenepal/financetracker/Config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.pennywisenepal.financetracker.Config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Order(1)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
