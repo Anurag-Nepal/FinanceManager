1c1aecd3ea0735018603799beb2b78eb24262926
Anurag-Nepal <anuragnepal99@gmail.com>
2024-08-23 09:26:07 +0545

    docker

--- src/main/java/com/pennywisenepal/financetracker/Config/SecurityConfig.java
+++ src/main/java/com/pennywisenepal/financetracker/Config/SecurityConfig.java
@@ -18,17 +18,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.net.http.HttpRequest;
 import java.util.Arrays;
 import java.util.List;
 
@@ -89,6 +84,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 
     @Bean
