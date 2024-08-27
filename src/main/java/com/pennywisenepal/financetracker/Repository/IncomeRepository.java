fd4088b8ceee8706daf35d40f5f029a048ccae76
Anurag-Nepal <anuragnepal99@gmail.com>
2024-08-27 09:19:06 +0545

    double changes

--- src/main/java/com/pennywisenepal/financetracker/Repository/IncomeRepository.java
+++ src/main/java/com/pennywisenepal/financetracker/Repository/IncomeRepository.java
@@ -19,7 +19,7 @@ public interface IncomeRepository extends JpaRepository<Income,Integer> {
     LocalDate thirtyDaysAgo = LocalDate.now().minusDays(30);
 
     @Query("SELECT SUM(i.iamount) FROM Income i WHERE i.user.username = :username AND i.idate >= :thirtyDaysAgo")
-    double getIncomeLastThirtyDaysForUserUsername(@Param("username") String username, @Param("thirtyDaysAgo") LocalDate thirtyDaysAgo);
+    Double getIncomeLastThirtyDaysForUserUsername(@Param("username") String username, @Param("thirtyDaysAgo") LocalDate thirtyDaysAgo);
 
     List<Income> findByUserUsernameAndIdateBetweenOrderByIdateDesc(String username, LocalDate startDate, LocalDate endDate);
 
